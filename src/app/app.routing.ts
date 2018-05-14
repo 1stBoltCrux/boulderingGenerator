3eaee361de4cd85559dd93a13dfa14c014a2aeb3
Garnett Puett and Isaac Shoman <garnett@>
2018-05-14 09:24:41 -0700

    add routing for each component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,26 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { GridComponent } from './grid/grid.component';
+import { ProblemListComponent } from './problem-list/problem-list.component';
+import { HelpComponent } from './help/help.component';
 
 const appRoutes: Routes = [
   {
     path: '',
     component: WelcomeComponent
+  },
+  {
+    path: 'grid',
+    component: GridComponent
+  },
+  {
+    path: 'problem-list',
+    component: ProblemListComponent
+  },
+  {
+    path: 'help',
+    component: HelpComponent
   }
 ];
 
